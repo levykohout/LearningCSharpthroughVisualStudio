a6da5bceb7b90711867bc2974aabd9a7613155f3
levykohout <levy.kohout@yahoo.com>
2017-02-13 19:56:17 -0600

    added automatically implemented properties

---
+++ AutomaticallyImplementedProperties/AutomaticallyImplementedProperties/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticallyImplementedProperties
+{
+     class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product (string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product() { }
+
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product>
+            {
+                new Product {Name = "West Side Story", Price=9.99m },
+                new Product {Name = "Assasins", Price=14.99m },
+                new Product {Name = "Frogs", Price=13.99m },
+                new Product {Name = "Sweeney Todd", Price=10.99m },
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}", Name, Price);
+        }
+    }
+}
